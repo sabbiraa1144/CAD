6bb7f132349789fa5c3cca2e3d19c41e7f207899
sabbiraa1144 <sabbirahamed5511@gmail.com>
2026-06-27 12:15:10 +0600

    Initial commit

---
+++ App.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+# Load trained model
+model = tf.keras.models.load_model("custom_cnn.keras")
+
+st.title("Coronary Artery Disease Detection")
+
+uploaded_file = st.file_uploader(
+    "Upload CT Scan Image",
+    type=["jpg", "jpeg", "png"]
+)
+
+if uploaded_file is not None:
+
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", width=300)
+
+    # Preprocess
+    img = image.resize((224, 224))
+    img = np.array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
+
+    if st.button("Predict"):
+
+        prediction = model.predict(img)
+
+        if prediction[0][0] > 0.5:
+            st.success("Prediction: Normal")
+        else:
+            st.error("Prediction: Abnormal")
